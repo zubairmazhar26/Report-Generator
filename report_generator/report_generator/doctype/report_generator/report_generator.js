e8deab65feaed2f47f46ec6d0e327906be492d3f
MuhammadZubair12 <zubairmazhar23@gmail.com>
2022-11-20 05:15:31 +0000

    updates

--- report_generator/report_generator/doctype/report_generator/report_generator.js
+++ report_generator/report_generator/doctype/report_generator/report_generator.js
@@ -13,19 +13,52 @@ frappe.ui.form.on('Report Generator', {
 		frm.doc.check = "0"
 	},
 	select_doctype: function(frm) {
+		frm.doc.sales_document_no = ""
 		frm.save()
 	},
 
-	fetch_record: function(frm) {
-		
-		if (frm.doc.select_doctype == "Sales Invoice") {
-			frm.call("getinvoice")
-		} else{
-			frm.call("getpinvoice")
+	fetch_record: function (frm) {
+
+		if (frm.doc.select_doctype == "Serial No") {
+			frm.call("getrecord")
 		}
+		else if (frm.doc.select_doctype == "Sales Invoice") {
+			frm.call("getinvoice")
+		} else if (frm.doc.select_doctype == "Purchase Invoice") {
+			frm.call("getpinvoice")
+		} else { }
 		frm.save()
-		
-		
+
+
+	},
+
+	fetch_record_pi: function (frm) {
+
+		if (frm.doc.select_doctype == "Serial No") {
+			frm.call("getrecord")
+		}
+		else if (frm.doc.select_doctype == "Sales Invoice") {
+			frm.call("getinvoice")
+		} else if (frm.doc.select_doctype == "Purchase Invoice") {
+			frm.call("getpinvoice")
+		} else { }
+		frm.save()
+
+
+	},
+	fetch_record_sn: function (frm) {
+
+		if (frm.doc.select_doctype == "Serial No") {
+			frm.call("getrecord")
+		}
+		else if (frm.doc.select_doctype == "Sales Invoice") {
+			frm.call("getinvoice")
+		} else if (frm.doc.select_doctype == "Purchase Invoice") {
+			frm.call("getpinvoice")
+		} else { }
+		frm.save()
+
+
 	},
 	te: function(frm) {
 		frm.save()
@@ -33,10 +66,37 @@ frappe.ui.form.on('Report Generator', {
 	s_status: function(frm) {
 		frm.save()
 	},
+	ser_status: function(frm) {
+		frm.save()
+	},
 	serial_no: function(frm) {
 		frm.save()
 	},
 	item_code: function(frm) {
 		frm.save()
-	}
+	},
+
+
+	sales_date: function(frm) {
+		frm.save()
+	},
+
+	sales_document_no: function(frm) {
+		frm.save()
+	},
+	supplier: function(frm) {
+		frm.save()
+	},
+	creation_document_no: function(frm) {
+		frm.save()
+	},
+	barcode: function(frm) {
+		frm.save()
+	},
+	serial_no: function(frm) {
+		frm.save()
+	},
+	serial_no: function(frm) {
+		frm.save()
+	},
 });
--- report_generator/report_generator/doctype/report_generator/report_generator.py
+++ report_generator/report_generator/doctype/report_generator/report_generator.py
@@ -11,22 +11,32 @@ class ReportGenerator(Document):
 	def getrecord(self):
 		self.report_record_details = ""
 		value = {}
-		conditions = " "
-		if (self.te and self.item_code):
-			doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` where status=%s and item_code=%s """,(self.te, self.item_code))
-			value = doc
-		elif (self.te):
-			doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` where status=%s """,(self.te))
-			value = doc
+		conditions = ""
+		if (self.item_code and self.ser_status and self.serial_no and self.creation_document_no):
+			conditions +=f"""where item_code='{self.item_code}' and status='{self.ser_status}' and serial_no='{self.serial_no}' and creation_document_no='{self.creation_document_no}'"""
+		elif (self.item_code and self.ser_status and self.serial_no):
+			conditions +=f"""where item_code='{self.item_code}' and status='{self.ser_status}' and serial_no='{self.serial_no}' """
+		elif (self.item_code and self.ser_status and self.creation_document_no):
+			conditions +=f"""where item_code='{self.item_code}' and status='{self.ser_status}' and creation_document_no='{self.creation_document_no}'"""
+		elif (self.item_code and self.creation_document_no):
+			conditions +=f"""where item_code='{self.item_code}' and status='{self.ser_status}' and creation_document_no='{self.creation_document_no}'"""
+		elif (self.item_code and self.ser_status):
+			conditions +=f"""where item_code='{self.item_code}' and status='{self.ser_status}'"""
+		elif (self.ser_status and self.creation_document_no):
+			conditions +=f"""where status='{self.ser_status}' and creation_document_no='{self.creation_document_no}'"""
+		elif (self.ser_status):
+			conditions +=f"""where status='{self.ser_status}' """
 		elif (self.item_code):
-			doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` where item_code=%s """,(self.item_code))
-			value = doc
+			conditions +=f"""where item_code='{self.item_code}'"""
+		elif (self.creation_document_no):
+			conditions +=f"""where creation_document_no='{self.creation_document_no}'"""
 		elif (self.serial_no):
-			doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` where serial_no=%s """,(self.serial_no))
-			value = doc
+			conditions +=f"""where serial_no='{self.serial_no}' """
 		else:
 			doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` """)
 			value = doc
+		doc = frappe.db.sql(""" select item_code,item_name,warehouse,batch_no,status,upload_image,name,company from `tabSerial No` %s"""%conditions)
+		value = doc
 		for a in value:
 			row = self.append("report_record_details", {})
 			row.item_code = a[0]
@@ -41,78 +51,129 @@ class ReportGenerator(Document):
 	def getpinvoice(self):
 		self.report_record_details = ""
 		value = {}
-		conditions = " "
-		if (self.item_code and self.te):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where si.item_code=%s and i.status=%s""",(self.item_code, self.te))
-			value = doc
+		conditions = ""
+		if (self.item_code and self.te and self.serial_no and self.supplier and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and si.serial_no='{self.serial_no}' and i.supplier='{self.supplier}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.te and self.serial_no and self.supplier):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and si.serial_no='{self.serial_no}' and i.supplier='{self.supplier}'"""
+		elif (self.item_code and self.te and self.serial_no and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and si.serial_no='{self.serial_no}' and i.name='{self.sales_document_no}'"""
+		elif (self.item_code and self.te and self.supplier and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and i.supplier='{self.supplier}' and i.name='{self.sales_document_no}'"""
+		elif (self.item_code and self.te and self.serial_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and si.serial_no='{self.serial_no}' """
+		elif (self.item_code and self.te and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.te and self.supplier):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' and i.supplier='{self.supplier}' """
+		elif (self.te and self.serial_no):
+			conditions +=f"""where i.status='{self.te}' and si.serial_no='{self.serial_no}' """
+		elif (self.te and self.supplier):
+			conditions +=f"""where i.status='{self.te}' and i.supplier='{self.supplier}' """
+		elif (self.te and self.sales_document_no):
+			conditions +=f"""where i.status='{self.te}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.te):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{self.te}' """
+		elif (self.item_code and self.supplier):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.supplier='{self.supplier}' """
 		elif(self.te):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where i.status=%s""",(self.te))
-			value = doc
+			conditions +=f"""where i.status='{self.te}' """
 		elif(self.item_code):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where si.item_code=%s """,(self.item_code))
-			value = doc
+			conditions +=f"""where si.item_code='{self.item_code}' """
 		elif (self.serial_no):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where si.serial_no=%s""",(self.serial_no))
-			value = doc
+			conditions +=f"""where si.serial_no='{self.serial_no}' """
+		elif (self.supplier):
+			conditions +=f"""where i.supplier='{self.supplier}' """
+		elif (self.sales_document_no):
+			conditions +=f"""where i.name='{self.sales_document_no}' """
 		else:
 			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where i.status=%s""",(self.te))
+			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name,i.supplier from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name where i.status=%s""",(self.te))
 			value = doc
+		doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
+		si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name,i.supplier from `tabPurchase Invoice Item` as si left join `tabPurchase Invoice` as i  ON si.parent=i.name %s"""%conditions)
+		value = doc
 		for v in value:
-			row = self.append("report_record_details", {})
-			row.serial_no = v[0]
-			row.item_code = v[1]
-			row.item_name = v[2]
-			row.delivered_qty = v[3]
-			row.upload_image = v[4]
-			row.warehouse = v[5]
-			row.qty = v[6]
-			row.rate = v[7]
-			row.precious_stone = v[8]
-			row.semi_precious_stone = v[9]
-			row.material_use_abbv = v[10]
-			row.stone_size = v[11]
-			row.cz = v[12]
-			row.diamond_size = v[13]
-			row.weight = v[14]
-			row.price_list_rate = v[15]
-			row.sales_date = v[16]
-			row.status = self.te
+			if v[0]:
+				row = self.append("report_record_details", {})
+				row.serial_no = v[0]
+				row.item_code = v[1]
+				row.item_name = v[2]
+				row.delivered_qty = v[3]
+				row.upload_image = v[4]
+				row.warehouse = v[5]
+				row.qty = v[6]
+				row.rate = v[7]
+				row.precious_stone = v[8]
+				row.semi_precious_stone = v[9]
+				row.material_use_abbv = v[10]
+				row.stone_size = v[11]
+				row.cz = v[12]
+				row.diamond_size = v[13]
+				row.weight = v[14]
+				row.price_list_rate = v[15]
+				row.sales_date = v[16]
+				row.supplier = v[18]
+				row.status = self.te
+			else:
+				pass
 	@frappe.whitelist(allow_guest=True)
 	def getinvoice(self):
 		self.report_record_details = ""
 		value = {}
-		conditions = " "
+		conditions = ""
 		if (self.s_status=="Delivered"):
 			n_status = "Paid"
 			s_status = "Delivered"
 		else:
 			n_status = self.s_status
 			s_status = self.s_status
-		if (self.item_code and self.s_status):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name where si.item_code=%s and i.status=%s""",(self.item_code, n_status))
-			value = doc
+		if (self.item_code and self.s_status and self.serial_no and self.barcode and self.sales_date and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.serial_no='{self.serial_no}' and si.barcode='{self.barcode}' and i.posting_date='{self.sales_date}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.s_status and self.serial_no and self.sales_date):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.serial_no='{self.serial_no}' and i.posting_date='{self.sales_date}' """
+		elif (self.item_code and self.s_status and self.serial_no and self.sales_document_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.serial_no='{self.serial_no}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.s_status and self.sales_document_no and self.sales_date):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and i.name='{self.sales_document_no}' and i.posting_date='{self.sales_date}' """
+		elif (self.item_code and self.s_status and self.barcode and self.sales_date):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.barcode='{self.barcode}' and i.posting_date='{self.sales_date}' """
+		elif (self.item_code and self.s_status and self.sales_date):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and i.posting_date='{self.sales_date}' """
+		elif (self.item_code and self.s_status and self.serial_no):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.serial_no='{self.serial_no}' """
+		elif (self.item_code and self.s_status and self.barcode):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' and si.barcode='{self.barcode}' """
+		elif (self.s_status and self.sales_date and self.sales_document_no):
+			conditions +=f"""where i.status='{n_status}' and i.posting_date='{self.sales_date}' and i.name='{self.sales_document_no}' """
+		elif (self.s_status and self.serial_no):
+			conditions +=f"""where i.status='{n_status}' and si.serial_no='{self.serial_no}' """
+		elif (self.s_status and self.sales_date):
+			conditions +=f"""where i.status='{n_status}' and i.posting_date='{self.sales_date}' """
+		elif (self.s_status and self.barcode):
+			conditions +=f"""where i.status='{n_status}' and si.barcode='{self.barcode}' """
+		elif (self.s_status and self.sales_document_no):
+			conditions +=f"""where i.status='{n_status}' and i.name='{self.sales_document_no}' """
+		elif (self.item_code and self.s_status):
+			conditions +=f"""where si.item_code='{self.item_code}' and i.status='{n_status}' """
 		elif(self.s_status):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name where i.status=%s""",(n_status))
-			value = doc
+			conditions +=f"""where i.status='{n_status}' """
 		elif(self.item_code):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name where si.item_code=%s """,(self.item_code))
-			value = doc
+			conditions +=f"""where si.item_code='{self.item_code}' """
 		elif (self.serial_no):
-			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
-			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name where si.serial_no=%s""",(self.serial_no))
-			value = doc
+			conditions +=f"""where si.serial_no='{self.serial_no}' """
+		elif (self.barcode):
+			conditions +=f"""where si.barcode='{self.barcode}' """
 		else:
 			doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
 			si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name where i.status='Paid'""")
 			value = doc
+		doc = frappe.db.sql(""" select si.serial_no,si.item_code,si.item_name,si.delivered_qty,si.image,si.warehouse,si.qty,si.rate,si.precious_stone,si.semi_precious_stone,\
+		si.material_use_abbv,si.stone_size,si.cz,si.diamond_size,si.weight,si.price_list_rate,i.posting_date,i.name from `tabSales Invoice Item` as si left join `tabSales Invoice` as i  ON si.parent=i.name %s"""%conditions)
+		value = doc
+		self.total_rate = 0
+		self.total_qty = 0
+		self.grand_total = 0
 		for v in value:
 			if v[0]:
 				row = self.append("report_record_details", {})
@@ -134,5 +195,8 @@ class ReportGenerator(Document):
 				row.price_list_rate = v[15]
 				row.sales_date = v[16]
 				row.status = s_status
+				self.total_rate += v[15]
+				self.total_qty += v[6]
+				self.grand_total += v[6] * v[7]
 			else:
 				pass
